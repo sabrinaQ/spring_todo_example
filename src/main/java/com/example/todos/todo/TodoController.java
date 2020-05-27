4a7fe4173a0b06ac7c0077f51e13e2a01f1ac52e
Sabrina Quintanilla <slq170030@gmail.com>
2020-05-27 16:25:08 -0500

    Initial commit

---
+++ src/main/java/com/example/todos/todo/TodoController.java
@@ -0,0 +1,56 @@
+package com.example.todos.todo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/todos")
+public class TodoController {
+
+    private TodoRepository todoRepository;
+
+    public TodoController(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    //gets all "findAll"
+    @GetMapping
+    public List<Todo> getTodos(){
+        return todoRepository.findAll();
+    }
+    //gets multiple "findByID"
+    @GetMapping("/{id}")
+    public Optional<Todo> getTodo(@PathVariable long id) {
+        return todoRepository.findById(id);
+    }
+
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo todo){
+        todoRepository.save(todo);
+        return todo;
+    }
+
+    @PutMapping
+    public Todo editTodo(@PathVariable long id, @RequestBody Todo todo){
+        Todo existingTodo = new Todo();
+        try{
+            existingTodo = todoRepository.findById(id).orElseThrow();
+            existingTodo.setTitle(todo.getTitle());
+            existingTodo.setDescription(todo.getDescription());
+            existingTodo.setComplete(todo.isComplete());
+        } catch(NoSuchElementException e) {
+            System.out.println(e.getMessage());
+            existingTodo = todoRepository.save(todo);
+        }
+        return existingTodo;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable long id){
+        todoRepository.deleteById(id);
+    }
+
+}
